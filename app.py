79f4f7922e2537509f5ec208ac187c3aad298b0f
Astra Clelia Bertelli <astraberte9@gmail.com>
2024-11-26 15:37:25 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,40 @@
+import gradio as gr
+import atproto as atp
+from text_summarization import text_inference
+
+
+client = atp.Client()
+
+
+def get_auth(username: str, password: str) -> bool:
+    global client
+    try:
+        client.login(username, password)
+        return True
+    except Exception as e:
+        return False
+
+def get_feed():
+    strtort = ""
+    strsum = ""
+    # Get "Home" page. Use pagination (cursor + limit) to fetch all posts
+    timeline = client.get_timeline(algorithm='reverse-chronological')
+    for feed_view in timeline.feed:
+        action = 'New Post🆕'
+        if feed_view.reason:
+            action_by = feed_view.reason.by.handle
+            action = f'Reposted by @{action_by}🔁'
+
+        post = feed_view.post.record
+        author = feed_view.post.author
+
+        strtort += f'### {action}\n\n<img alt="Avatar for {author.display_name}" src="{author.avatar}" width=50> **{author.display_name}**:\n\n{post.text}\n\n-------------------\n\n'
+        strsum += f'Author: {author.display_name} - Post content: {post.text}'
+    chat_hist = [{"role": "system", "content": "You are a helpful and efficient feed summarization assistant. You should provide a coincise and well-style, polished overview of what the post authors are telling"}, {"role": "user", "content": strsum}]
+    res = text_inference(chat_hist)
+    smry = f"\n\n<details>\n\t<summary><b>Feed Summary</b></summary>\n\n{res}\n\n</details>\n\n"
+    strtort = f"<div align='center'>\n\t<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Bluesky_Logo.svg/1920px-Bluesky_Logo.svg.png' alt='BlueSky Logo' width=150>\n</div>\n<br>\n\n## Home Feed (Following)🏠\n\n----------------------\n\n{smry}\n\n{strtort}"
+    return strtort
+
+iface = gr.Interface(fn=get_feed, inputs=None, outputs=gr.Markdown(), title="BlueSky User Feed", theme='shivi/calm_seafoam')
+iface.launch(auth=get_auth, auth_message="Insert here your BlueSky username/handle and your password")
---
+++ text_summarization.py
@@ -0,0 +1,41 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+import accelerate
+
+import torch
+from transformers import pipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig  
+
+
+model_name = "meta-llama/Llama-3.2-3B-Instruct"
+quantization_config = BitsAndBytesConfig(load_in_4bit=True,
+    bnb_4bit_compute_dtype=torch.bfloat16,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type= "nf4"
+)
+
+quantized_model = AutoModelForCausalLM.from_pretrained(model_name, device_map="cuda:0", torch_dtype=torch.bfloat16,quantization_config=quantization_config)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+
+pipe = pipeline(
+    "text-generation", 
+    model=quantized_model, 
+    tokenizer=tokenizer,
+    device_map="cuda:0"
+)
+
+
+def text_inference(chat_hist):
+    prompt = chat_hist
+    res = pipe(
+        prompt,
+        do_sample=False,
+        temperature=0,
+        top_p=1,
+        max_new_tokens=512,
+        repetition_penalty=1.8
+    )
+    ret = res[0]["generated_text"]
+    return ret[-1]["content"]
